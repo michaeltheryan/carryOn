eb3f2682a8f49562fbb37dacd234b3cee2c27d0d
Michael Ryan <michaeltheryan@gmail.com>
2024-06-24 13:51:37 -0400

    creation of initial directory structure and html files

---
+++ src/main/java/org/theryan/carryon/CarryOnApplication.java
@@ -0,0 +1,13 @@
+package org.theryan.carryon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarryOnApplication.class, args);
+    }
+
+}
