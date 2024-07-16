1803f7741b014eedf5894af3728cf4373360c4b5
Michael Ryan <michaeltheryan@gmail.com>
2024-07-16 11:13:42 -0400

    project creation from spring initializr

---
+++ src/main/java/com/michaelryan/carryon/CarryonApplication.java
@@ -0,0 +1,13 @@
+package com.michaelryan.carryon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryonApplication.class, args);
+	}
+
+}
